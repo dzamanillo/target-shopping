1e1c81b395c6e783553896cb770077f23872b1a6
Daniel Zamanillo <dzamanillok@gmail.com>
2022-01-18 14:57:18 -0600

    server/server: production

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Serve up static assets
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
